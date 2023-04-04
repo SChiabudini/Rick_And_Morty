661c821a894e0029ba0fd66c7efc256ac2d2f8a3
SChiabudini <sofiachiabudini@gmail.com>
2023-04-03 23:05:27 -0300

    first commit

---
+++ src/components/Cards.jsx
@@ -0,0 +1,23 @@
+import Card from './Card';
+
+export default function Cards({characters}) {
+   return (
+      <div>
+         {characters.map(({id, name, status, species, gender, origin, image}) =>{
+            return(
+               <Card
+                  key={id}/*React usa esta info de manera interna*/
+                  id={id}
+                  name={name}
+                  status={status}
+                  species={species}
+                  gender={gender}
+                  origin={origin.name}
+                  image={image}
+                  onClose={() => alert('Emulamos que se cierra la card')}
+               />
+            )
+         })}
+      </div>
+   );
+}
