4a86bdf30bb5bc8ab4f338968c6236cbb066c34b
SChiabudini <sofiachiabudini@gmail.com>
2023-04-06 13:39:16 -0300

    Estados y lifeCycle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import './App.css';
-import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import Nav from './components/Nav.jsx';
+import {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+   const [characters, setCharacters] = useState([]);
+
+   const example = {
+      id: 1,
+      name: 'Rick Sanchez',
+      status: 'Alive',
+      species: 'Human',
+      gender: 'Male',
+      origin: {
+         name: 'Earth (C-137)',
+         url: 'https://rickandmortyapi.com/api/location/1',
+      },
+      image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+   };
+
+   function onSearch(id) {
+      axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   function onClose(id){
+      const charactersFiltered = characters.filter(character => character.id !== Number(id))
+      setCharacters(charactersFiltered)
+   }
+
    return (
       <div className='App'>
-         <SearchBar onSearch={(characterID) => alert(characterID)} />
-         <Cards characters={characters} />
-         <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => alert('Emulamos que se cierra la card')}
-         />
+         <Nav onSearch={onSearch}/>
+         <Cards characters={characters} onClose={onClose}/>
       </div>
    );
 }
