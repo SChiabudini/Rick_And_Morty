741eb6f52f83f9bc292d0fc3819bfda8dd7a8403
SChiabudini <sofiachiabudini@gmail.com>
2023-05-05 18:34:22 -0300

    DB in progress

---
+++ Server/src/DB_connection.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const FavoriteModel = require('./models/Favorite');
+const UserModel = require('./models/User');
+
+const sequelize = new Sequelize(`postgress://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+   { logging: false, native: false }
+);
+
+FavoriteModel(sequelize);
+UserModel(sequelize);
+
+// Ejercicio 06
+// ¡Relaciona tus modelos aquí abajo!
+// const { User, Favorite } = sequelize.models;
+
+module.exports = {
+   // User,
+   // Favorite,
+   conn: sequelize,
+};
